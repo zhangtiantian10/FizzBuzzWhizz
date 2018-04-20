683548c29a1849a43fd4fb059a64cd9aecf5b5ef
zhang tian <975571814@qq.com>
2018-04-20 22:43:07 +0800

    should return Fizz when have 3

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -5,15 +5,23 @@ import java.util.List;
 
 
 public class FizzBuzzGame {
-    private List<String> results = new ArrayList<>();
+  private List<String> results = new ArrayList<>();
 
-    public void start(int count) {
+  public void start(int count) {
+    for (int i = 1; i <= count; i++) {
+      String text = String.format("%d", i);
+      if (text.contains("3")) {
+        results.add("Fizz");
+        continue;
+      }
 
+      results.add(text);
     }
+  }
 
-    public List<String> getResults() {
-        return results;
-    }
+  public List<String> getResults() {
+    return results;
+  }
 }
 
 
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -2,9 +2,17 @@ package com.thoughtworks;
 
 import org.junit.Test;
 
-public class FizzBuzzGameTest {
-    @Test
-    public void should_return_Fizz_when_have_3() {
+import java.util.List;
 
-    }
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+
+public class FizzBuzzGameTest {
+  @Test
+  public void should_return_Fizz_when_have_3() {
+    FizzBuzzGame game = new FizzBuzzGame();
+    game.start(3);
+    List<String> results = game.getResults();
+    assertThat(results.get(2), equalTo("Fizz"));
+  }
 }
