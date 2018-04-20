e45a5e9f7320e0665085eb546608c3eab6702cc0
zhang tian <975571814@qq.com>
2018-04-20 23:27:11 +0800

    fix wrong expect value

--- src/test/java/com/thoughtworks/integrationTest/FizzBuzzGameIntegrationTest.java
+++ src/test/java/com/thoughtworks/integrationTest/FizzBuzzGameIntegrationTest.java
@@ -17,15 +17,15 @@ public class FizzBuzzGameIntegrationTest {
     public void testFizzBuzzGameResults() {
         List<String> expectedResults = Arrays.asList(
                 "1", "2", "Fizz", "4", "Buzz", "Fizz", "Whizz", "8", "Fizz", "Buzz",
-                "11", "Fizz", "Fizz", "Whizz", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz",
-                "FizzWhizz", "22", "Fizz", "Fizz", "Buzz", "26", "Fizz", "Whizz", "29", "Fizz",
-                "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Buzz",
-                "41", "FizzWhizz", "Fizz", "44", "FizzBuzz", "46", "47", "Fizz", "Whizz", "Buzz",
-                "Fizz", "52", "Fizz", "Fizz", "Buzz", "Whizz", "Fizz", "58", "59", "FizzBuzz",
-                "61", "62", "Fizz", "64", "Buzz", "Fizz", "67", "68", "Fizz", "BuzzWhizz",
-                "71", "Fizz", "Fizz", "74", "FizzBuzz", "76", "Whizz", "Fizz", "79", "Buzz",
+                "11", "Fizz", "Fizz", "Whizz", "FizzBuzz", "16", "Whizz", "Fizz", "19", "Buzz",
+                "FizzWhizz", "22", "Fizz", "Fizz", "Buzz", "26", "Fizz", "Whizz", "29", "FizzBuzz",
+                "Fizz", "Fizz", "Fizz", "Fizz", "BuzzWhizz", "Fizz", "Fizz", "Fizz", "Fizz", "Buzz",
+                "41", "FizzWhizz", "Fizz", "44", "FizzBuzz", "46", "Whizz", "Fizz", "Whizz", "Buzz",
+                "Fizz", "Buzz", "Fizz", "Fizz", "Buzz", "Whizz", "Fizz", "Buzz", "Buzz", "FizzBuzz",
+                "61", "62", "FizzWhizz", "64", "Buzz", "Fizz", "Whizz", "68", "Fizz", "BuzzWhizz",
+                "Whizz", "Fizz", "Fizz", "Whizz", "FizzBuzz", "Whizz", "Whizz", "Fizz", "Whizz", "Buzz",
                 "Fizz", "82", "Fizz", "FizzWhizz", "Buzz", "86", "Fizz", "88", "89", "FizzBuzz",
-                "Whizz", "92", "Fizz", "94", "Buzz", "Fizz", "97", "Whizz", "Fizz", "Buzz"
+                "Whizz", "92", "Fizz", "94", "Buzz", "Fizz", "Whizz", "Whizz", "Fizz", "Buzz"
         );
 
 
